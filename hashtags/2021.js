201073b6b730a32f188421c9d026d6aa0e1ee19f
Marc Görtz <mail@marcgoertz.de>
2021-07-26 15:21:18 +0200

    Add season 2021/2022.

---
+++ hashtags/2021.js
@@ -0,0 +1,22 @@
+const hashtags = [
+	'#B04',
+	'#BMG',
+	'#BOC',
+	'#BSC',
+	'#BVB',
+	'#DSC',
+	'#FCA',
+	'#FCB',
+	'#FCU',
+	'#HOF',
+	'#KOE',
+	'#M05',
+	'#RBL',
+	'#SCF',
+	'#SGE',
+	'#SGF',
+	'#VFB',
+	'#WOB',
+];
+
+export default hashtags;
--- hashtags/index.js
+++ hashtags/index.js
@@ -3,6 +3,7 @@ import h2017 from './2017.js';
 import h2018 from './2018.js';
 import h2019 from './2019.js';
 import h2020 from './2020.js';
+import h2021 from './2021.js';
 
 const allHashtags = {
 	2016: h2016,
@@ -10,6 +11,7 @@ const allHashtags = {
 	2018: h2018,
 	2019: h2019,
 	2020: h2020,
+	2021: h2021,
 };
 
 export default allHashtags;
